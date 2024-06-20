9a09a73280e8990b4031ea88bdaf208ce1e00927
grafica <paolo@monema.it>
2024-06-20 18:41:20 +0200

    componente parallax

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -3,6 +3,7 @@ import styles from "./page.module.scss";
 import { unstable_setRequestLocale } from "next-intl/server";
 import { getTranslations } from "next-intl/server";
 import heroData from "../../../public/data/hero.json";
+import Parallax from "@/components/parallax/parallax";
 
 /**
  * PAGINA
@@ -24,6 +25,7 @@ export default async function Home({
     <main className={styles.main}>
       {t("titolo")}
       <HeroVideo videoURL={"/video/vts.mp4"} data={HeroDataLang} />
+      <Parallax imageURL="/image/mainimage.jpg" alt="test" height="300px" />
     </main>
   );
 }
